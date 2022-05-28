9b196c56c166d459f30308cf02183f0ce7000f44
jixiu.zlw <jixiu.zlw@antgroup.com>
2022-05-28 15:43:43 +0800

    feat: add __execCommand support

--- test/macaca-playwright.test.js
+++ test/macaca-playwright.test.js
@@ -5,14 +5,15 @@ const assert = require('power-assert');
 
 const _ = require('../lib/helper');
 const Playwright = require('../lib/macaca-playwright');
+const consoleProcess = require('./scripts/consoleProcess');
 
 describe('unit testing', function() {
   this.timeout(5 * 60E3);
+  const customUserAgent = 'custom userAgent';
 
   describe('methods testing', function() {
 
     const driver = new Playwright();
-    const customUserAgent = 'custom userAgent';
 
     before(async () => {
       const videoDir = path.resolve(__dirname, '..', 'videos');
@@ -113,6 +114,79 @@ describe('unit testing', function() {
 
     after(async () => {
       await driver.stopDevice();
+    });
+  });
+
+  describe('__execCommand test', function() {
+    const driver = new Playwright();
+
+    before(async () => {
+      const videoDir = path.resolve(__dirname, '..', 'videos');
+      await driver.startDevice({
+        headless: true,
+        uitest: true,
+        userAgent: customUserAgent,
+        recordVideo: {
+          dir: videoDir,
+        },
+      });
+    });
+
+    it('test __execCommand unknonw command', async () => {
+      const result = await driver.execute('__execCommand("keyboard1", "press", "KeyA")');
+      assert.equal(result, null);
+    });
+
+    it('test __execCommand invalid command', async () => {
+      const result = await driver.execute('__execCommand(null, "press", "KeyA")');
+      assert.equal(result, null);
+    });
+
+    it('test commands keyboard should work', async () => {
+      await driver.get('file://' + path.resolve(__dirname, 'webpages/4.html'));
+      await _.sleep(1000);
+      const box = await driver.findElement('id', 'target');
+      const boxText = await driver.getText(box.ELEMENT);
+      assert.equal(boxText, 'macaca');
+      const boxClick = await driver.findElement('id', 'target_click');
+      const clickText = await driver.getText(boxClick.ELEMENT);
+      assert.equal(clickText, 'mousemovemousedown');
+    });
+
+    after(async () => {
+      await driver.stopDevice();
+    });
+  });
+
+  describe('waitForClose test', function() {
+    it('waitForClose should work', async () => {
+      const driver = new Playwright();
+      await driver.startDevice({
+        uitest: true,
+        headless: true,
+        userAgent: customUserAgent,
+      });
+      setTimeout(() => {
+        driver.stopDevice();
+      }, 0);
+      await driver.waitForClose();
+      assert(true);
+    });
+  });
+
+  describe('console redirect', function() {
+  
+    it('console test', async () => {
+      const types = [ 'log', 'warn', 'error', 'info' ];
+      for (const type of types) {
+        const data = await consoleProcess(type, 'macaca');
+        assert.equal(data, 'macaca');
+      }
+    });
+
+    it('console at page throws an error or a warning', async () => {
+      const data = await consoleProcess('load5');
+      assert(data.startsWith('Fetch API cannot load'));
     });
   });
 });
---
+++ test/scripts/consoleProcess.js
@@ -0,0 +1,36 @@
+'use strict';
+
+if (process.argv[2] === 'child') {
+  const path = require('path');
+  const Playwright = require('../../lib/macaca-playwright');
+  const driver = new Playwright();
+  (async () => {
+    await driver.startDevice({
+      headless: true,
+      uitest: true,
+    });
+    if (process.argv[3] === 'load5') {
+      await driver.get('file://' + path.resolve(__dirname, '../webpages/5.html'));
+    } else {
+      await driver.execute(`console.${process.argv[3]}('${process.argv[4]}')`);
+    }
+    await driver.stopDevice();
+  })();
+  
+} else {
+  const { fork } = require('child_process');
+
+  module.exports = function(type, value) {
+    const child = fork(__filename, [ 'child', type, value ], { silent: true });
+    const data = [];
+
+    child.stdout.on('data', chunk => data.push(chunk));
+    child.stderr.on('data', chunk => data.push(chunk));
+
+    return new Promise((resolve) => {
+      child.on('exit', () => {
+        resolve(data.join('').trimEnd());
+      });
+    });
+  };
+}
